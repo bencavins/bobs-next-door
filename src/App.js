3f5cab7a701ee6380ffc5e48902abd165bb5fa0f
Ben Cavins <ben.cavins@flatironschool.com>
2023-09-18 14:36:49 -0400

    do lab

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import Search from './components/Search'
 import NewStoreForm from './components/NewStoreForm';
 import StoreList from './components/StoreList';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [stores, setStores] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:8085/stores")
+    .then(resp => resp.json())
+    .then(data => setStores(data))
+  }, [])
+
+  const filteredStores = stores.filter(store => {
+    return store.name.toLowerCase().includes(
+      search.toLowerCase()
+    ) || String(store.season).includes( // not required
+      search
+    )
+  })
+
   return (
     <div className="main-container">
       <img src="/images/bobsburgers.png" />
       <h1>Neighbor Stores</h1>
-      <Search />
-      <NewStoreForm />
-      <StoreList />
+      <Search setSearch={setSearch} />
+      <NewStoreForm setStores={setStores} />
+      <StoreList stores={filteredStores} />
     </div>
   );
 }
